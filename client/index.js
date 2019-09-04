461bbff2f616f7e95ac226edf1e4b1ccec0b509e
Hugo <contacthugocampos@gmail.com>
2019-09-04 13:18:10 -0700

    remove Provider if no store exists

--- client/index.js
+++ client/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
 import App from './components/App';
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>, root);
+ReactDOM.render(<App/>, root);
