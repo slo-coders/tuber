2bf6f758b288a1acf76a0e8121e71bdc37fe67d3
Thom Smith <thom.smith2@gmail.com>
2019-09-08 23:17:35 -0700

    linter got me

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Model
-const { User, Meetup, UserMeetup } = require('../db/index');
+const { User, UserMeetup } = require('../db/index');
 
 // Routes
 router
