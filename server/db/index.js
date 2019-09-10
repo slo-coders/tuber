68fd0b1e41711df0960c2252c5e1ff37a44fb18f
Hugo <contacthugocampos@gmail.com>
2019-09-09 19:31:05 -0700

    simplify import-export of models and db

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,14 @@
 const db = require('./db');
 
-const {
-  User,
-  Course,
-  CourseTopic,
-  Topic,
-  UserTopic,
-  Meetup,
-  UserMeetup,
-} = require('./models/index');
+// const {
+//   User,
+//   Course,
+//   CourseTopic,
+//   Topic,
+//   UserTopic,
+//   Meetup,
+//   UserMeetup,
+// } = require('./models/index');
 
-module.exports = { db, User, Course, CourseTopic, Topic, UserTopic, Meetup, UserMeetup };
+// module.exports = { db, User, Course, CourseTopic, Topic, UserTopic, Meetup, UserMeetup };
+module.exports = {...require('./models/index'), db};
