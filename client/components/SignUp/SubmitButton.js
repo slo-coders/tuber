0d649a0f4ecfa35c4a9117a60baee186e3eb4631
sstampher <stephanierstampher@gmail.com>
2019-09-10 12:59:25 -0700

    got SignUp page started

---
+++ client/components/SignUp/SubmitButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button() {
+  return (
+    <div className="buttons">
+      <a className="button is-primary">Submit</a>
+    </div>
+  );
+}
---
+++ client/components/SignUp/TopicSelect.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Topics from '../Topics';
+
+// This component is under construction, will be built out soon
+
+export default function TopicSelect() {
+  return <Topics />;
+}
