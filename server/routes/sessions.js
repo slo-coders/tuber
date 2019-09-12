470c4f21546e0886ef256f6098c72f9c4da4aada
SamOlivas <sam.a.olivas@gmail.com>
2019-09-11 22:46:23 -0700

    SID delivered, some seqlz errors

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -25,7 +25,6 @@ router.post('/login', async (req, res, next) => {
   }
   try {
     //LEFT OFF HERE
-    console.log('We tried');
     const loggedSessionUser = await User.findOne({
       where: {
         email: req.body.email,
