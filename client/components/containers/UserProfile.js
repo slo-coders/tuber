0f366fa68a4fe34fd78521f250cdb5474a4eca61
sstampher <stephanierstampher@gmail.com>
2019-09-14 18:20:55 -0700

    set up secondary scss file with compile

--- client/components/containers/UserProfile.js
+++ client/components/containers/UserProfile.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchLoggedInThunked } from '../../actions/sessionActions';
 
 class UserProfile extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {};
   }
 
+  componentDidMount(){
+    this.props.getLoggedInUser();
+  }
+
   render() {
+    console.log('logged in user info:', this.props.user.authUser ? this.props.user.authUser:'')
     return (
+      <div>
+      <div className="vertical1">
+        <div className="turquoise"/>
+          <hr/>
+        </div>
       <div>
         <span className="icon has-text-warning">
           <i className="fas fa-exclamation-triangle"></i>
         </span>
-        <h4>The Profile component is under construction.</h4>
+        <h4 className="turquoise">{this.props.user.authUser.firstName}</h4>
+      </div>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  user: state.auth
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getLoggedInUser: () => dispatch(fetchLoggedInThunked())
+  }
+}
+
 export default connect(
-  null,
-  null,
+  mapStateToProps,
+  mapDispatchToProps,
 )(UserProfile);
