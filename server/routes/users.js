e8c3f52b3fbeb7c0d0ff7812329562ee2e1875cd
Hugo <contacthugocampos@gmail.com>
2019-09-20 20:05:54 -0700

    simplify assigning of new average rating

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Model
 const {
   User,
-  // UserSession,
+  UserSession,
   UserMeetup,
   Meetup,
   UserTopic,
@@ -76,6 +76,7 @@ router
       next(err);
     }
   });
+
 //Gets all meetupinformation for a user.
 router.get('/:userId/meetups', async (req, res, next) => {
   try {
@@ -92,62 +93,63 @@ router.get('/:userId/meetups', async (req, res, next) => {
   }
 });
 
+/* TODO: route for updating a user's partner UserMeetup should probably be:
+ `/users/:partnerId/meetups/:meetupId` not `/users/:userId/meetups/:meetupId`
+ */
 router.put('/:userId/meetups/:meetupId', async (req, res, next) => {
   try {
-    //first user submits req {proficiencyRating: , ...rest} with PUT to /:userId/meetups/:meetupId
-    const userMeetupUpdate = await UserMeetup.updateUserMeetup(
+    //User submits req.body = {proficiencyRating:} with PUT to get UserMeetup instances for both themeselves and partner
+    const newPartnerProfRating = req.body.proficiencyRating;
+    const userMeetupUpdate = await UserMeetup.ratePartnerUserMeetup(
       req.params.userId,
       req.params.meetupId,
-      req,
+      newPartnerProfRating,
     );
-    //this route returns meetup array of users, which has both user's info and partner's info
-    const partner = userMeetupUpdate.filter(
-      user => user.userId !== req.params.userId,
-    )[0];
-    console.log('partner: ', partner);
-    console.log('req.params.meetupId: ', req.params.meetupId);
-    const topicId = await UserMeetup.findOne({
-      where: { meetupId: req.params.meetupId },
-      includes: [{ model: Topic, through: { model: MeetupTopic } }],
-    }); //.topics[0].id; //due to associations, topics is a property in UserMeetup
-    console.log('topicId: ', topicId);
+    
+    const {partnerMeetupInfo, personalMeetupInfo} = userMeetupUpdate;
 
-    //can get topidIc w/ meetupId
+    if(newPartnerProfRating && partnerMeetupInfo.proficiencyRating) {
+      //GET the meetup's topicId
+      const meetup = (
+        await Meetup.findOne({
+          where: {id: req.params.meetupId },
+          include: [
+            { model: Topic, through: { model: MeetupTopic } }
+          ],
+        })
+      );
+        
+      const topicId = meetup.topics[0].id;
+      
+      //GET the partner's previous running average
+      const prevRunningAveTopicProfeciency = (
+        await UserTopic.findOne({
+          where: {
+            userId: partnerMeetupInfo.userId,
+            topicId,
+          }
+        })
+      ).proficiencyRating;
+      
+      //Set an alpha for an estimated exponenial moving average
+      const alpha = 0.1;
 
-    //UserSession -> topidId = UserMeetup.topics[0].id -> UserTopic.profeciency
-    // const findTopicFromUserSession = await UserSession.findOne({
-    //   where: {
-    //     userId: partner.userId,
-    //   },
-    // });
+      //Calculate new running average
+      const newAveProfRating = alpha*newPartnerProfRating + (1 - alpha)*prevRunningAveTopicProfeciency;
 
-    // //updated partner profeciency info
-    // const meetupInfo = await User.findOne({
-    //   where: { id: partner.userId },
-    //   include: [
-    //     { model: Meetup, through: { model: UserMeetup } }, //status
-    //     { model: Topic, through: { model: UserTopic } }, //topic
-    //   ],
-    // });
+      //Update newAveProfRating in UserTopic table
+      await UserTopic.update({
+        proficiencyRating: newAveProfRating
+      }, {
+        where: {
+          userId: partnerMeetupInfo.userId,
+          topicId
+        }
+      });
 
-    // const runningTopicProfeciency = await UserMeetup.findAll({
-    //   where: {
-    //     id: partner.userId,
-    //     topicId: topicId,
-    //   },
-    //   includes: { model: UserMeetup },
-    // });
-
-    //request past 29 proficiency ratings from /api/users/${partnerId}/meetups (i.e., UserMeetups.getMostRecent model class method)
-    /*
-    const mostRecentProficiencyRatings = meetupInfo.topics
-      .sort((topicA, topicB) => topicA.updatedAt > topicB.updatedAt) //check sort method
-      .slice(0, 30)
-      .map(topic => topic.user_topic.proficiencyRating); //check user_topic or user_topics in res
-*/
-    //calculate average for user with new proficiencyRating:
-    //// const newProficiencyRating = 0.1*(partner.proficiencyRating) + 0.9*(user.proficiencyRating + UserMeetups.getMostRecent().reduce((a,b) => a + b, 0))
-    //PUT newProficiencyRating into /api/user/${partenerBeingRated.id}/topics/:topicId (i.e., UserTopic model)
+    } else {
+      console.log('Error; either could not find previous average rating or new rating could not be read correctly.');
+    }
     res.send(userMeetupUpdate);
   } catch (err) {
     next(err);
