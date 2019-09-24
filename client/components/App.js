c34b90a0695bb2818916c98fb1e53854777ea8de
Hugo <contacthugocampos@gmail.com>
2019-09-23 17:21:27 -0700

    remove chatroom component from App

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Home from './containers/Home';
-// import Chatroom from './reusables/Chatroom';
 import Nav from './containers/Nav';
 import UserProfile from './containers/UserProfile/UserProfile';
 import RequestMatch from './containers/RequestMatch/RequestMatch';
@@ -28,12 +27,11 @@ class App extends React.Component {
         <Route
           exact
           path="/request_match"
-          /* path will eventually depend on match status */ component={
+          component={
             RequestMatch
           }
         />
         <Route exact path="/meetuproom" component={MeetupRoom} />
-        {/* <Route exact path="/chatroom" component={Chatroom} /> */}
       </HashRouter>
     );
   }
